9a6089128703b2ce7730b260831ea8fd91dac686
Emmanuel chinonso <emmanuelchinons9@gmail.com>
2020-04-11 23:09:19 +0100

    challenge1 complete

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,28 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  let input = data;
+  const {reportedCases} = input;
+
+  function getCurrentlyInfected(factor) {
+    return reportedCases * factor;
+  }
+
+  function getNumberInfectedByTime(time) {
+    const {currentlyInfected} = this;
+    const factor = Math.trunc(time / 3);
+    return currentlyInfected * (2 ** factor);
+  }
+
+  return {
+    data: input,
+    impact: {
+      currentlyInfected: getCurrentlyInfected(10),
+      infectionsByRequestedTime: getNumberInfectedByTime
+    },
+    severeImpact: {
+      currentlyInfected: getCurrentlyInfected(50),
+      infectionsByRequestedTime: getNumberInfectedByTime
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
