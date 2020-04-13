b5786a095ec21443b4429fd098c33e0ef60730b5
Emmanuel chinonso <emmanuelchinons9@gmail.com>
2020-04-13 14:24:14 +0100

    add changes to challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,26 +1,32 @@
 const covid19ImpactEstimator = (data) => {
   const input = data;
-  const { reportedCases } = input;
+  const { reportedCases, timeToElapse, periodType } = input;
 
   function getCurrentlyInfected(factor) {
     return reportedCases * factor;
   }
 
   function getNumberInfectedByTime(time) {
-    const { currentlyInfected } = this;
-    const factor = Math.trunc(time / 3);
-    return currentlyInfected * (2 ** factor);
+    const factor;
+    const durationInDays = time;
+    if (periodType == 'weeks'){
+      durationInDays = time * 7;
+    }else if (periodType == 'months'){
+      durationInDays = time * 4 * 7;
+    }
+    factor = Math.trunc( durationInDays / 3 );
+    return 2 ** factor;
   }
 
   return {
     data: input,
     impact: {
       currentlyInfected: getCurrentlyInfected(10),
-      infectionsByRequestedTime: getNumberInfectedByTime
+      infectionsByRequestedTime: getCurrentlyInfected(10) * getNumberInfectedByTime(timeToElapse)
     },
     severeImpact: {
       currentlyInfected: getCurrentlyInfected(50),
-      infectionsByRequestedTime: getNumberInfectedByTime
+      infectionsByRequestedTime: getCurrentlyInfected(50) * getNumberInfectedByTime(timeToElapse)
     }
   };
 };
